95e19f915de5fd851b0a2dee8a3316b818d058f8
Harshit Thakur <thakurharshit.dev@gmail.com>
2026-08-09 10:07:16 +0530

    add login.spec.ts

---
+++ src/pages/login/login.spec.tsx
@@ -0,0 +1,20 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import LoginPage from "./login";
+
+describe("Login  Page", () => {
+  it("should render with required fields", () => {
+    //arrange,assert,act
+    render(<LoginPage />);
+    expect(screen.getByText("Sign in")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("username")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("password")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Log in" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", { name: "Remember me" }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("link", { name: "Forgot password" }),
+    ).toBeInTheDocument();
+  });
+});
