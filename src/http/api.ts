b68b50509c5c237d73efbecfc6dc5c5b07a2a0e1
Harshit Thakur <thakurharshit.dev@gmail.com>
2026-08-11 23:09:34 +0530

    add user table

--- src/http/api.ts
+++ src/http/api.ts
@@ -9,3 +9,5 @@ export const login = (credetials: loginCredentials) =>
 export const self = () => api.get("/auth/self");
 
 export const logout = () => api.post("/auth/logout");
+
+export const getUsers = () => api.get("/users");
